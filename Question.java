cf81118476065e6d855789c7de81456d2a2d185a
shivam240104 <mauryatanmay845@gmail.com>
2024-09-23 20:01:13 +0530

    to reverse a String using Stack

--- Question.java
+++ Question.java
@@ -10,16 +10,33 @@ public class Question {
         pushAtBottom(s, data);
         s.push(top);
     }
-    public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
-       
-        pushAtBottom(s,5);
-        while(!s.isEmpty()){
-            System.out.println(s.pop());
+    // Question-2 Reverse a string 
+    public static String reverseString(String str){
+        Stack<Character>s= new Stack<>();
+        int idx=0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
         }
+        StringBuilder result = new StringBuilder();
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+        // s.push(4);
+       
+        // pushAtBottom(s,5);
+        // while(!s.isEmpty()){
+        //     System.out.println(s.pop());
+        // }
+        String str="Shivam";
+        System.out.println(reverseString(str));
     }
 }
